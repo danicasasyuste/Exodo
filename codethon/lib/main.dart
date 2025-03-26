2c7a4e65e106344cc83d38e32b1694ba55ab7322
grim <jfalbujas@gmail.com>
2025-03-26 21:04:35 +0100

    prueba commit

--- codethon/lib/main.dart
+++ codethon/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'grim estuvo aca',
       theme: ThemeData(
         // This is the theme of your application.
         //
